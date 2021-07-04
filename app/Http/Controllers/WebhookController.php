e9aa8fdff163f2d719f513a8c4c8e00be18f175e
ducpq <quyducphan96@gmail.com>
2021-07-05 00:31:12 +0700

    fix

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -94,12 +94,11 @@ class WebhookController extends Controller
         // Check if the message contains text
         if($received_message['text']) {
             // Create the payload for a basic text message
-            $response['text'] = 'You sent the message: ' .  $received_message['text'].  '. Now send me an image!';
+            $response['text'] = 'Bạn đã gửi tin nhắn: ' .  $received_message['text'].  '. Now send me an image!';
         }
 
         // Sends the response message
         $this->callSendAPI($sender_psid, $response);
-
     }
 
     private function callSendAPI($sender_psid, $response) {
