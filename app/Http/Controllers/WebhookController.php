b771e3335f39eedada7b677bb5092c0155ed32f1
ducpq <quyducphan96@gmail.com>
2021-07-05 11:10:48 +0700

    fix

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -99,33 +99,35 @@ class WebhookController extends Controller
             // Gets the URL of the message attachment
             $attachment_url = $received_message['attachments'][0]['payload']['url'];
 
-            $response = [
-                'attachment' => [
-                    'type' => 'template',
-                    'payload' => [
-                        'template_type' => 'generic',
-                        'elements' => [
-                            [
-                                'title' => 'Is this the right picture?',
-                                'subtitle' => 'Tap a button to answer.',
-                                'image_url' => $attachment_url,
-                                'buttons' => [
-                                    [
-                                        'type' => 'postback',
-                                        'title' => 'Yes!',
-                                        'payload' => 'yes'
-                                    ],
-                                    [
-                                        'type' => 'postback',
-                                        'title' => 'No!',
-                                        'payload' => 'no'
-                                    ]
-                                ]
-                            ]
-                        ]
-                    ]
-                ]
-            ];
+            $response['text'] = $attachment_url;
+
+//            $response = [
+//                'attachment' => [
+//                    'type' => 'template',
+//                    'payload' => [
+//                        'template_type' => 'generic',
+//                        'elements' => [
+//                            [
+//                                'title' => 'Is this the right picture?',
+//                                'subtitle' => 'Tap a button to answer.',
+//                                'image_url' => $attachment_url,
+//                                'buttons' => [
+//                                    [
+//                                        'type' => 'postback',
+//                                        'title' => 'Yes!',
+//                                        'payload' => 'yes'
+//                                    ],
+//                                    [
+//                                        'type' => 'postback',
+//                                        'title' => 'No!',
+//                                        'payload' => 'no'
+//                                    ]
+//                                ]
+//                            ]
+//                        ]
+//                    ]
+//                ]
+//            ];
         }
 
         // Sends the response message
